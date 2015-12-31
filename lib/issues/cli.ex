86d657a79a257d227d10f8b29d74f8a32e804a26
Eric Hogberg <misc@erichogberg.com>
2015-12-30 18:45:20 -0600

    simplest thing that could possibly work.

---
+++ lib/issues/cli.ex
@@ -0,0 +1,65 @@
+defmodule Issues.CLI do
+	import Issues.TableFormatter, only: [print_table_for_columns: 2]
+	import Issues.GithubIssues, only: [fetch: 2]
+
+	@default_count 4
+
+	@module_doc """
+	Handle the command-line parsing and the dispatch
+	to the various functions that end up generating
+	a table of the last _n_ issues in a github project
+	"""
+
+
+	def main(argv) do
+		argv
+		|> parse_args
+		|> process
+	end
+
+
+	def process(:help) do
+		IO.puts """
+		usage: issues <user> <project> [count | #{@default_count}]
+		"""
+		System.halt(0)
+	end
+
+	def process({user,project,count}) do
+		fetch(user, project)
+		|> decode_response
+		|> convert_to_list_of_hashdicts
+		|> sort_into_ascending_order
+		|> Enum.take(count)
+		|> print_table_for_columns(["number","created_at","title"])
+	end
+
+	def sort_into_ascending_order(list_of_issues) do
+		Enum.sort(list_of_issues,
+			fn i1, i2 -> i1["created_at"] <= i2["created_at"] end)
+	end
+
+	def decode_response({:ok,body}), do: body
+
+	def convert_to_list_of_hashdicts(list) do
+		list
+		|> Enum.map(&Enum.into(&1, HashDict.new))
+	end
+
+	@doc """
+	'argv' can be -h or --help , which returns :help
+	Otherwise it is a Github user name.
+	"""
+	def parse_args(argv) do
+		parse = OptionParser.parse(argv,
+															 switches: [help: :boolean],
+															 aliases: [h: :help])
+
+		case parse do
+			{[help: true], _, _} -> :help
+			{_, [ user, project, count ], _} -> {user, project, String.to_integer(count)}
+			{_, [user,project], _} -> {user, project, @default_count}
+			_ -> :help
+		end
+	end
+end
--- mix.exs
+++ mix.exs
@@ -5,16 +5,21 @@ defmodule Issues.Mixfile do
     [app: :issues,
      version: "0.0.1",
      elixir: "~> 1.1",
+		 escript: escript_config,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
+	defp escript_config do
+		[main_module: Issues.CLI]
+	end
+
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger,:httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +32,8 @@ defmodule Issues.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:mix_test_watch, "~> 0.2", only: :dev},
+		 {:httpoison, "~> 0.4"},
+		 {:jsx, "~> 2.0"}]
   end
 end
